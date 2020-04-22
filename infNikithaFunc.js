6874fd3c8e8859bc7a52db62de9dc8d6f1589eaf
ramesh <rameshnagaraju@gmail.com>
2020-04-22 21:17:34 +0530

    update delete functionality added

--- infNikithaFunc.js
+++ infNikithaFunc.js
@@ -9,14 +9,10 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient({
   region: AWS_DEPLOY_REGION,
 });
 
-/*
-    Function to Add training
-*/
-
 module.exports.createTraining = async (event) => {
-  let _parsedJsonobj;
+  let parsedJsonobj;
   try {
-    _parsedJsonobj = JSON.parse(event.body);
+    parsedJsonobj = JSON.parse(event.body);
   } catch (err) {
     console.error(`Could not parse requested JSON ${event.body}: ${err.stack}`);
     return {
@@ -30,7 +26,7 @@ module.exports.createTraining = async (event) => {
     trainingName,
     trainingDescription,
     resourceName,
-  } = _parsedJsonobj;
+  } = parsedJsonobj;
 
   const params = {
     TableName: TRAININGS_TABLE,
@@ -45,7 +41,7 @@ module.exports.createTraining = async (event) => {
   try {
     const data = await dynamoDb.put(params).promise();
     console.log(`createTraining data=${JSON.stringify(data)}`);
-    return { statusCode: 200, body: JSON.stringify(params.Item) };
+    return { statusCode: 200, body: JSON.stringify(params.Item.trainingId) };
   } catch (error) {
     console.log(`createTraining ERROR=${error.stack}`);
     return {
@@ -106,27 +102,83 @@ module.exports.getTraining = async (event) => {
 */
 
 module.exports.updateTraining = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "updated Training",
-      },
-      null,
-      2
-    ),
+  // Validate If the path has training Id
+  if (!event.pathParameters.trainingId) {
+    return {
+      statusCode: 404,
+      error: `No trainingId in path parameter: ${JSON.stringify(
+        event.pathParameters
+      )}`,
+    };
+  }
+
+  // Validate that the body has parameters to update
+  let parsedJsonobj;
+  try {
+    parsedJsonobj = JSON.parse(event.body);
+  } catch (err) {
+    console.error(`Could not parse requested JSON ${event.body}: ${err.stack}`);
+    return {
+      statusCode: 400,
+      error: `Could not find parameters to update : ${err.stack}`,
+    };
+  }
+
+  //  Get the parameter name and parameter value to be updated
+
+  const paramsName = parsedJsonobj.paramName;
+  const paramsValue = parsedJsonobj.paramValue;
+  const trainingId = event.pathParameters.trainingId;
+
+  const params = {
+    TableName: TRAININGS_TABLE,
+    Key: { trainingId },
+    ConditionExpression: "attribute_exists(trainingId)",
+    UpdateExpression: "set " + paramsName + " = :v",
+    ExpressionAttributeValues: {
+      ":v": paramsValue,
+    },
+    ReturnValues: "ALL_NEW",
   };
+
+  try {
+    const data = await dynamoDb.update(params).promise();
+    return { statusCode: 200, body: JSON.stringify(data) };
+  } catch (err) {
+    console.log(`update Training ERROR=${error.stack}`);
+    return {
+      statusCode: 400,
+      error: `Could not update Training: ${error.stack}`,
+    };
+  }
 };
 
 module.exports.deleteTraining = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Deleted Training",
-      },
-      null,
-      2
-    ),
+  // Validate Inputs
+  if (!event.pathParameters.trainingId) {
+    return {
+      statusCode: 404,
+      error: `No trainingId in path parameter: ${JSON.stringify(
+        event.pathParameters
+      )}`,
+    };
+  }
+
+  // We have valid input, Now prepare the params for Database delete operation
+  const params = {
+    TableName: TRAININGS_TABLE,
+    Key: { trainingId: event.pathParameters.trainingId },
   };
+
+  // Delete from DB
+  try {
+    const data = await dynamoDb.delete(params).promise();
+    return { statusCode: 200, body: JSON.stringify("Training  Deleted") };
+  } catch (error) {
+    console.log(`delete Training ERROR=${error.stack}`);
+    return {
+      statusCode: 400,
+      error: `Could not delete Training: ${error.stack}`,
+    };
+  }
 };
